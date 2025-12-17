0134c35d1a9a037ac27fbd98bd0dd3a6b2f9cd71
Berfin Gündoğan <berfingundogan00@gmail.com>
2025-12-17 15:33:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7794648938-bab60",
+  "appId": "1:138621825454:web:8066e15f7cfe71fdb4183f",
+  "apiKey": "AIzaSyD7FHD8S2dKeqEVNMB7EsGNmI1MqYP9Z2Q",
+  "authDomain": "studio-7794648938-bab60.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "138621825454"
+};

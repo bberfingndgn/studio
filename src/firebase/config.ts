a11948143902243bf57be05262e24afc54286137
Berfin Gündoğan <berfingundogan00@gmail.com>
2026-01-02 12:41:46 +0000

    const firebaseConfig = {   apiKey: "AIzaSyD7FHD8S2dKeqEVNMB7EsGNmI1MqYP9

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD7FHD8S2dKeqEVNMB7EsGNmI1MqYP9Z2Q",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-7794648938-bab60.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-7794648938-bab60",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-7794648938-bab60.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "138621825454",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:138621825454:web:8066e15f7cfe71fdb4183f",
 };
